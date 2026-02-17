8cf6b30961866bd2c8b23d33eec76638f68b965f
Zbr123 <syedzubairalam123@gmail.com>
2026-02-17 17:49:14 +0500

    fix: add sequelize-cli for documentation

--- config/pg-config.js
+++ config/pg-config.js
@@ -4,9 +4,7 @@ const pg = require("pg");
 dotenv.config();
 
 // Use DATABASE_URL if defined, else build from individual env vars
-const dbUrl =
-  process.env.DATABASE_URL ||
-  `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const dbUrl = process.env.DATABASE_URL;
 
 const sequelize = new Sequelize(dbUrl, {
   dialect: "postgres",
