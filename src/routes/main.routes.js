ce8d6c194aeb894eda214ba01f3a605db7eb4659
Zbr123 <syedzubairalam123@gmail.com>
2026-02-17 20:25:49 +0500

    test: add test url

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,7 +1,9 @@
 //all routes will be gathered here
 const authRoutes = require('./auth.routes')
+const testRoutes = require('./test.routes')
 
 const routes = [
     ...authRoutes,
+    ...testRoutes
 ]
 module.exports = { routes }
