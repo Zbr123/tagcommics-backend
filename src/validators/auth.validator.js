4a79c3a377b4d399e303badc3303f2f6fe274d13
Zbr123 <syedzubairalam123@gmail.com>
2026-02-19 14:35:50 +0500

    chore: update phone as optional

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const registerValidatorSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    phone: Joi.string().required(),
+    phone: Joi.optional(),
     password: Joi.string().min(8).required()
 });
 
